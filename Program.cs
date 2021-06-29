d200c19d7d8c5802472fb4943e96c0008ebe7fa1
fact-desa-01\desarrolladorP <paul.tipantuna@heaven.com.ec>
2021-06-29 12:48:40 -0500

    save information of the ruc ok

--- Program.cs
+++ Program.cs
@@ -5,27 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
-
-
-
 namespace INFORMATION
 {
     class Program
     {
-
-       
-  
-
-
         static void Main(string[] args)
         {
             ClsInformationCompany information1 = new ClsInformationCompany();
             information1.sendinformation();
-        }
-
-      
+        } 
     }
     
     
