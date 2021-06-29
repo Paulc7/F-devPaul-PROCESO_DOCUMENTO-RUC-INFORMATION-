d0789854dccc6b855b945a18f30f1f8d1e56cef5
fact-desa-01\desarrolladorP <paul.tipantuna@heaven.com.ec>
2021-06-29 12:46:40 -0500

    save information of the ruc

---
+++ Models/ClsInformationCompany.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+namespace INFORMATION.Models
+{
+    class ClsInformationCompany
+    {
+        public void sendinformation()
+        {
+            string vgurl = ConfigurationSettings.AppSettings["ruc"];
+            string path = @"F:\devPaul\PROCESO_DOCUMENTO\1724000516001\TXTINFORMATION\" + vgurl + ".txt";
+            string name = "SIPER",
+                   address = "LA VICENTINA VERDE CRUZ E16-13 Y PADRE JACOME";
+            if (!File.Exists(path))
+            {
+                using(StreamWriter read = File.CreateText(path))
+                {
+                    read.WriteLine(vgurl);
+                    read.WriteLine(name);
+                    read.WriteLine(address);
+                }
+            }
+            
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using INFORMATION.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+
 
 
 
@@ -11,9 +15,14 @@ namespace INFORMATION
     class Program
     {
 
+       
+  
+
+
         static void Main(string[] args)
         {
-
+            ClsInformationCompany information1 = new ClsInformationCompany();
+            information1.sendinformation();
         }
 
       
